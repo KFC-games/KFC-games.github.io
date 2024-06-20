a658254a9d89b83bf45bb1df70a38d717eb647e5
12 <135738687+KFC-games@users.noreply.github.com>
2024-06-20 02:50:51 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+var sortAscending = false;
+const sortbtn = document.getElementById('sortToggle');
+const gamesContainer = document.getElementById("games-container");
+const originalOrder = Array.from(gamesContainer.getElementsByClassName("game"));
+const gamesCount = document.getElementById("count");
+
+
+console.log("Whatever you're doing in here is probably not going to work since you are not very smart (no offense) :(");
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+function filterGames() {
+    const searchInput = document.querySelector('.search-input');
+    const games = gamesContainer.getElementsByClassName('game');
+    const searchTerm = searchInput.value.toLowerCase();
+
+    for (const game of games) {
+        const gameText = game.querySelector('.gametxt').textContent.toLowerCase();
+        if (gameText.includes(searchTerm)) {
+            game.style.display = 'block';
+        } else {
+            game.style.display = 'none';
+        }
+    }
+}
+
+function toggleSort() {
+    var games = Array.from(gamesContainer.getElementsByClassName("game"));
+
+    sortAscending = !sortAscending;
+
+    if (sortAscending) {
+        sortbtn.innerHTML = "A-Z";
+        games.sort((a, b) => a.querySelector('.gametxt').textContent.localeCompare(b.querySelector('.gametxt').textContent));
+    } else {
+        sortbtn.innerHTML = "New - Old";
+        games = originalOrder.slice();
+    }
+
+    gamesContainer.innerHTML = "";
+    games.forEach(game => {
+        gamesContainer.appendChild(game);
+    });
+}
+
+function closeAlert() {
+    const alertBox = document.querySelector(".alert");
+    alertBox.style.display = "none";
+}
+
+function pickRandom() {
+    rnum = getRndInteger(0, parseInt(gamesCount.textContent))
+    rnum--;
+    game = originalOrder[rnum];
+
+    var htmlString = game.innerHTML;
+    var tempElement = document.createElement('div');
+    tempElement.innerHTML = htmlString;
+    
+    var link = tempElement.querySelector('a').getAttribute('href');
+    window.location = link;
+}
